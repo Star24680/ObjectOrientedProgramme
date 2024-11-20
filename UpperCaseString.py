88895df5717474124c1ab995b1a6770524b20e60
Star24680 <gulerumaan1@gmail.com>
2024-11-20 17:50:22 +0500

    first commit

---
+++ UpperCaseString.py
@@ -0,0 +1,10 @@
+class IOString():
+    def __init__(self):
+        self.str1=""
+    def GetString(self):
+        self.str1=input("ENTER STRING: ")
+    def PrintStr (self):
+        print("The Result is ",self.str1.upper())
+str1=IOString()
+str1.GetString
+str1.PrintStr
